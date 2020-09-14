305420552822c93c4a39e6275007e7ded2515d5f
Robert Kuraj <robert.kuraj@applandeo.pl>
2020-09-14 09:16:49 +0200

    Create project

---
+++ StanwoodApp/StanwoodApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StanwoodApp
+//
+//  Created by Robert  Kuraj on 14/09/2020.
+//  Copyright © 2020 Robert Kuraj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
